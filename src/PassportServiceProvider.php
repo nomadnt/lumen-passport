bffe7ebcad6df0dc9289a4eee2b44a13b4994747
armando-moreira <armando.moreira@gdcgroup.com>
2025-01-27 12:51:37 +0000

    Update passport service provider

--- src/PassportServiceProvider.php
+++ src/PassportServiceProvider.php
@@ -8,17 +8,34 @@ use Laravel\Passport\PassportServiceProvider as LaravelPassportServiceProvider;
 class PassportServiceProvider extends LaravelPassportServiceProvider
 {
     /**
-     * Bootstrap the application services.
+     * Register the Passport routes.
      *
      * @return void
      */
-    public function boot()
+    protected function registerRoutes()
     {
-        // Remove the routes registration as they should be done in a different way
-        $this->registerResources();
-        $this->registerPublishing();
-        $this->registerCommands();
+        if (Passport::$registersRoutes) {
+            $this->app->router->group([
+                'as' => 'passport.',
+                'prefix' => config('passport.path', 'oauth'),
+                'namespace' => 'Laravel\Passport\Http\Controllers',
+            ], function ($router) {
+                require __DIR__.'/../routes/web.php';
+            });
+        }
+    }
 
-        $this->deleteCookieOnLogout();
+    /**
+    * Register the service provider.
+    *
+    * @return void
+    */
+    public function register()
+    {
+        parent::register();
+        $this->app->singleton(
+            \Illuminate\Contracts\Routing\ResponseFactory::class,
+            \Laravel\Lumen\Http\ResponseFactory::class
+        );
     }
 } 
