92ebed37844765215d72bfa54afef3d029a91339
armando-moreira <armando.moreira@gdcgroup.com>
2025-01-28 10:37:24 +0000

    Add some comments to explain code

--- src/PassportServiceProvider.php
+++ src/PassportServiceProvider.php
@@ -33,6 +33,7 @@ class PassportServiceProvider extends LaravelPassportServiceProvider
     public function register()
     {
         parent::register();
+        // This binding is necessary for Passport to work properly with Lumen, as it tells Lumen's container which implementation to use for the ResponseFactory contract.
         $this->app->singleton(
             \Illuminate\Contracts\Routing\ResponseFactory::class,
             \Laravel\Lumen\Http\ResponseFactory::class
